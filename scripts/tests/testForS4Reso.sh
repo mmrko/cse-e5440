10c8bb181818c239903ff2556351dbe1c4825298
Casuldo <kasper.apajalahti@gmail.com>
2016-04-25 19:42:11 +0300

    '.'

---
+++ scripts/tests/testForS4Reso.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+
+# For  768x1280, S4 is 1080x1920
+# => 1.40625x1.5
+# Twitter
+
+input tap 281 150 # 281 150
+sleep 5
+input text "https://twitter.com/"
+sleep 5
+input keyevent 66
+sleep 5
+input tap 984 300 #Menu 984 300
+sleep 5
+input tap 492 1200 #About 492 1200
+sleep 5
+
+#Yahoo
+input tap 281 150 # 281 150
+sleep 5
+input text "https://www.yahoo.com/"
+sleep 5
+input keyevent 66
+sleep 5
+input tap 70 300 #Menu 70 300
+sleep 5
+input tap 140 1200 #Sports 140 1200
+sleep 5
+
+#Flickr
+input tap 281 150 # 281 150
+sleep 5
+input text "https://flickr.com"
+sleep 5
+input keyevent 66
+sleep 5
+input tap 70 300 # 70 300
+sleep 5
+input tap 70 450 # Explore 70 450
+
+#Weather
+#input tap 281 150 # 281 150
+#sleep 5
+#input text "https://weather.com"
+#sleep 5
+#input keyevent 66
+#sleep 5
+#input tap 1055 300 #Menu? 1055 300
+#sleep 5
+#input tap 140 1200
