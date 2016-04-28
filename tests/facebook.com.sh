149aac488233ee4bf9cbed2385d2160d2f84d1fc
Lucas0T <zoo.triefenbach@gmx.de>
2016-04-28 14:49:56 +0300

    facebook added

--- tests/facebook.com.sh
+++ tests/facebook.com.sh
@@ -1,41 +1,90 @@
 #!/bin/sh
 #Facebook
 
-input tap 400 400 # usr textbox
-sleep 2
-input text "zoo.lutrie@gmx.de" #usr
-sleep 2
-
-input tap 400 540 # pw textbox
-sleep 2
-
-input text "frosch" # pw
-sleep 2
+alias tap="input tap"
+alias swipe="input touchscreen swipe"
+alias text="input text"
 
 
-input tap 400 600 # login button
-sleep 5
+swipe_params_1="250 1050 250 250 200"
+swipe_params_2="250 250 250 1150 180"
+BASE_SWIPE_WAIT_TIME="2"
+BASE_SUBPAGE_NAVIGATION_WAIT_TIME="5"
+
+#tap 400 400 && sleep $BASE_SWIPE_WAIT_TIME # usr textbox
+#text "zoo.lutrie@gmx.de" && sleep $BASE_SWIPE_WAIT_TIME
 
 
-input tap 200 1100 #enter no 4-digit code for phone
-sleep 3
+#tap 400 540 # pw textbox
+#sleep $BASE_SWIPE_WAIT_TIME
 
-input touchscreen swipe 200 200 200 700 3000 # swipe down
-sleep 2
-
-input touchscreen swipe 200 700 200 200 3000 # swipe up
-sleep 2
-
-input tap 710 200 # enter menu
-sleep 3
-
-input tap 710 400 # choose notifications
-sleep 3
+#text "frosch" # pw
+#sleep $BASE_SWIPE_WAIT_TIME
 
 
-input touchscreen swipe 200 200 200 700 2000 # Replace with better swipe-up command
-sleep 2
+#tap 400 600 # login button
+#sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
+
+#tap 200 1100 #enter no 4-digit code for phone
+#sleep BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+swipe $swipe_params_2 # swipe up
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_2 # swipe up
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+tap 710 200 # enter menu
+sleep $(($BASE_SWIPE_WAIT_TIME*2))
+
+tap 710 600 # choose events
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+
+
+swipe $swipe_params_1 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+swipe $swipe_params_2 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+tap 710 200 # enter menu
+sleep $BASE_SWIPE_WAIT_TIME
+
+tap 710 700 # choose pages
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+
+
+swipe $swipe_params_1 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+swipe $swipe_params_2 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
+
+tap 710 200 # enter menu
+sleep BASE_SWIPE_WAIT_TIME
+
+tap 710 800 # choose trending
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+
+
+swipe $swipe_params_1 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+swipe $swipe_params_2 # Replace with better swipe-up command
+sleep $BASE_SWIPE_WAIT_TIME
 
 
 
