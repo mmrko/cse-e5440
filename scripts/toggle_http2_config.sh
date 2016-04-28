9a696bbb0e3beea62a0b667589e65a1c78074369
Marko Raatikka <marko.raatikka@synack.com>
2016-04-28 15:25:16 +0300

    Fix toggle_http2_config.sh. Increase sleep times

--- scripts/toggle_http2_config.sh
+++ scripts/toggle_http2_config.sh
@@ -2,12 +2,11 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source "$SCRIPT_DIR/vars"
-export $(cut -d= -f1 "$SCRIPT_DIR/vars")
 
 alias input="adb shell input"
 
 adb shell "am start $PACKAGE_NAME" >/dev/null && adb shell sleep 2
-BASE_NAVIGATION_WAIT_TIME=1 $SCRIPT_DIR/navigate_to.sh "about:config"
+$SCRIPT_DIR/navigate_to.sh "about:config"
 input tap 700 300 >/dev/null
 input text "enabled.http2" >/dev/null
 adb shell sleep 1
