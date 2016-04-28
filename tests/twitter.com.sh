4d1b0b5cc094faa1d2ae0a00a1dc952140e5c42d
Casuldo <kasper.apajalahti@gmail.com>
2016-04-28 17:41:42 +0300

    '.'

--- tests/twitter.com.sh
+++ tests/twitter.com.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
 #Twitter
-#540 x 960
-input tap 200 50
-input text "https://twitter.com/"
-input keyevent KEYCODE_ENTER
-sleep 15
-input tap 500 100 #Menu
-sleep 15
-input tap 300 580 #Search
-sleep 15
-input tap 250 360 #Search field
-input text "obama"
-input keyevent KEYCODE_ENTER
-sleep 15
-input touchscreen swipe 200 700 200 300 # Replace with better swipe-down command
-sleep 5
-input touchscreen swipe 200 300 200 700 # Replace with better swipe-up command
-sleep 5
-input tap 500 100 #Menu
-sleep 5
-input tap 300 650 #About
-sleep 15
-input touchscreen swipe 200 700 200 300 # Replace with better swipe-down command
-sleep 5
-input touchscreen swipe 200 300 200 700 # Replace with better swipe-up command
-sleep 5
+
+navigate_to "https://twitter.com/"
+$((BASE_TAP_WAIT_TIME * 3))
+
+navigate_to "https://mobile.twitter.com/search"
+$((BASE_TAP_WAIT_TIME * 3))
+
+navigate_to https://mobile.twitter.com/search?q=obama
+$((BASE_TAP_WAIT_TIME * 3))
+
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+
+
+navigate_to https://mobile.twitter.com/search?q=aalto
+$((BASE_TAP_WAIT_TIME * 3))
+
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+
+navigate_to https://about.twitter.com/
+$((BASE_TAP_WAIT_TIME * 3))
+
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+
+
+
+
--- tests/yahoo.com.sh
+++ tests/yahoo.com.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 #Yahoo
-#540 x 960
-input tap 200 50
-sleep 5
-input text "https://www.yahoo.com/"
-sleep 5
-input keyevent KEYCODE_ENTER
-sleep 15
-input touchscreen swipe 200 700 200 400 # Replace with better swipe-down command
-sleep 5
-input touchscreen swipe 200 400 200 700 # Replace with better swipe-up command
-sleep 5
-input tap 45 115 #Menu 70 300
-sleep 5
-input tap 145 560 #Sports 140 1200
-sleep 15
-input touchscreen swipe 200 700 200 400 # Replace with better swipe-down command
-sleep 5
-input touchscreen swipe 200 400 200 700 # Replace with better swipe-up command
-sleep 5
+
+navigate_to "https://www.yahoo.com/"
+$((BASE_TAP_WAIT_TIME * 3))
+
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+
+navigate_to "http://sports.yahoo.com/"
+$((BASE_TAP_WAIT_TIME * 3))
+
+navigate_to "http://sports.yahoo.com/news"
+$((BASE_TAP_WAIT_TIME * 3))
+
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+
+navigate_to "http://sports.yahoo.com/soccer/premier-league/"
+$((BASE_TAP_WAIT_TIME * 3))
