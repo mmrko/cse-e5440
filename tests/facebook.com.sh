7d340f24339c6eb9b1c83ffbc07a607106f02a2f
Lucas0T <zoo.triefenbach@gmx.de>
2016-04-28 15:08:28 +0300

    Facebook checked again it is working on the emulator. I wanted to verify some stuff for instagramm now. My firefox is crashing with instragram on the emulator, so I only scroll down atm.

--- tests/facebook.com.sh
+++ tests/facebook.com.sh
@@ -11,23 +11,23 @@ swipe_params_2="250 250 250 1150 180"
 BASE_SWIPE_WAIT_TIME="2"
 BASE_SUBPAGE_NAVIGATION_WAIT_TIME="5"
 
-#tap 400 400 && sleep $BASE_SWIPE_WAIT_TIME # usr textbox
-#text "zoo.lutrie@gmx.de" && sleep $BASE_SWIPE_WAIT_TIME
+tap 400 400 && sleep $BASE_SWIPE_WAIT_TIME # usr textbox
+text "zoo.lutrie@gmx.de" && sleep $BASE_SWIPE_WAIT_TIME
 
 
-#tap 400 540 # pw textbox
-#sleep $BASE_SWIPE_WAIT_TIME
+tap 400 540 # pw textbox
+sleep $BASE_SWIPE_WAIT_TIME
 
-#text "frosch" # pw
-#sleep $BASE_SWIPE_WAIT_TIME
-
-
-#tap 400 600 # login button
-#sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+text "frosch" # pw
+sleep $BASE_SWIPE_WAIT_TIME
 
 
-#tap 200 1100 #enter no 4-digit code for phone
-#sleep BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+tap 400 600 # login button
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
+
+
+tap 200 1100 #enter no 4-digit code for phone
+sleep BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
 swipe $swipe_params_1 # swipe down
 sleep $BASE_SWIPE_WAIT_TIME
--- tests/instagram.com.sh
+++ tests/instagram.com.sh
@@ -2,46 +2,51 @@
 #Instagram
 
 
-input tap 200 50
-sleep 5
-input text "https://www.instagram.com"
-sleep 5
-input keyevent KEYCODE_ENTER
-sleep 5
-input tap 300 750 # 70 300
-sleep 2
+
+alias tap="input tap"
+alias swipe="input touchscreen swipe"
+alias text="input text"
 
 
-input touchscreen swipe 200 400 200 700 3000 # swipe down
-sleep 2
-
-input touchscreen swipe 200 400 200 700 3000 # swipe down
-sleep 2
-
-input touchscreen swipe 200 400 200 700 3000 # swipe down
-sleep 2
+swipe_params_1="250 1050 250 250 200" #down
+swipe_params_2="250 250 250 1150 180" #up
+BASE_SWIPE_WAIT_TIME="2"
+BASE_SUBPAGE_NAVIGATION_WAIT_TIME="5"
 
 
 
 
-input touchscreen swipe 200 700 200 400 3000 #swipe up
-sleep 2
+#input tap 200 50
+#sleep 5
+#input text "https://www.instagram.com"
+#sleep 5
+#input keyevent KEYCODE_ENTER
+#sleep 5
 
-input touchscreen swipe 200 700 200 400 3000 # swipe up
-sleep 2
 
-input touchscreen swipe 200 700 200 400 3000 # swipe up
-sleep 2
+tap 300 750 # 70 300
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
-input tap 200 300 #tap search button
-sleep 2
 
-input tap 500 440 #tap search textfield
-sleep 2
-input text "instagram"
-sleep 2
-input tap 400 680 #tap found
-sleep 2
 
-input touchscreen swipe 200 400 200 600 4000 # swipe up
-sleep 2
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+swipe $swipe_params_1 # swipe down
+sleep $BASE_SWIPE_WAIT_TIME
+
+
