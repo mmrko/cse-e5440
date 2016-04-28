34ee3eeebab0fae45fb977faaea9055ccfe18118
Marko Raatikka <marko.raatikka@synack.com>
2016-04-28 10:11:18 +0300

    Assign swipe params as a variable

--- tests/weather.com.sh
+++ tests/weather.com.sh
@@ -4,11 +4,13 @@
 alias tap="input tap"
 alias swipe="input touchscreen swipe"
 
+swipe_params_1="450 1050 250 250 200"
+
 # navigate to an article at the bottom of the page
-swipe 450 1050 250 250 200 && sleep $BASE_SWIPE_WAIT_TIME
-swipe 450 1050 250 250 200 && sleep $BASE_SWIPE_WAIT_TIME
-swipe 450 1050 250 250 200 && sleep $BASE_SWIPE_WAIT_TIME
-swipe 450 1050 250 250 200 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 tap 450 450 && sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
 # open menu & navigate to All Photos
@@ -17,5 +19,5 @@ tap 450 1400 && sleep $BASE_TAP_WAIT_TIME
 tap 450 1560 && sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
 # navigate to an article
-swipe 450 1050 250 250 100 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 tap 450 1150 && sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
