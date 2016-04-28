df0008126beda3c65a143ddf0150bfb192c5671a
Marko Raatikka <marko.raatikka@synack.com>
2016-04-28 21:43:27 +0300

    Fix Instagram

--- tests/instagram.com.sh
+++ tests/instagram.com.sh
@@ -6,31 +6,33 @@ alias swipe="input touchscreen swipe"
 alias text="input text"
 alias navigate_to="sh ./helpers/navigate_to"
 
-tap 300 1100 # 70 300
-sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
-
 swipe_params_1="250 1050 250 250 200"
-swipe_params_2="250 250 250 1150 180"
 
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 navigate_to "https://www.instagram.com/dano_compel/"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+tap 450 1550
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 navigate_to "https://www.instagram.com/binghuiliu/"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
-swipe $swipe_params_2 && sleep $BASE_SWIPE_WAIT_TIME
+tap 450 1550
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 navigate_to "https://www.instagram.com/tietokilta/"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
-swipe $swipe_params_2 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
+tap 450 1550
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
 navigate_to "https://www.instagram.com/pvyshnav/"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 
-swipe $swipe_params_2 && sleep $BASE_SWIPE_WAIT_TIME
-swipe $swipe_params_2 && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params_1 && sleep $BASE_SWIPE_WAIT_TIME
--- tests/yahoo.com.sh
+++ tests/yahoo.com.sh
@@ -16,6 +16,7 @@ navigate_to "$base_url/tech"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
+swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 
 navigate_to "$base_url/news"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
