00391284ebebf5cacfe06d3d175ab0e1cf0ab34c
Marko Raatikka <marko.raatikka@gmail.com>
2016-04-30 01:54:26 +0300

    Compute energy consumption from 22s to 395s (ignore OS/browser start/end idle)

--- scripts/R/energy_consumption.R
+++ scripts/R/energy_consumption.R
@@ -8,8 +8,8 @@ df_http2_1 = read.csv('./data/http2_run1_power_over_time.csv')
 df_http2_2 = read.csv('./data/http2_run3_power_over_time.csv')
 df_http2_3 = read.csv('./data/http2_run4_power_over_time.csv')
 
-df_http1 = c(df_http1_1[,2], df_http1_2[,2], df_http1_3[,2])
-df_http2 = c(df_http2_1[,2], df_http2_2[,2], df_http2_3[,2])
+df_http1 = c(df_http1_1[seq(22*5000,395*5000), 2], df_http1_2[seq(22*5000,395*5000), 2], df_http1_2[seq(22*5000,395*5000), 2])
+df_http2 = c(df_http2_1[seq(22*5000,395*5000), 2], df_http2_2[seq(22*5000,395*5000), 2], df_http2_2[seq(22*5000,395*5000), 2])
 
 sink("stats.txt", append=FALSE, split=FALSE)
 
@@ -19,6 +19,9 @@ cat(mean(df_http1))
 cat('\n')
 cat('Standard deviation: ')
 cat(sd(df_http1))
+cat('\n')
+cat('Sum: ')
+cat(sum(df_http1) / 3 / 5000)
 
 cat('\n\n')
 
@@ -28,6 +31,9 @@ cat(mean(df_http2))
 cat('\n')
 cat('Standard deviation: ')
 cat(sd(df_http2))
+cat('\n')
+cat('Sum: ')
+cat(sum(df_http2) / 3 / 5000)
 cat('\n')
 
 sink()
