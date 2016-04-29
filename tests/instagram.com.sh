25454f6a362487c03d1ad7c3cbb397485021411d
Marko Raatikka <marko.raatikka@synack.com>
2016-04-29 11:45:56 +0300

    Fix Instagram

--- tests/instagram.com.sh
+++ tests/instagram.com.sh
@@ -9,13 +9,10 @@ alias navigate_to="sh ./helpers/navigate_to"
 base_url="https://instagram.com"
 swipe_params="250 1050 250 250 200"
 
-navigate_to "$base_url/dano_compel/"
+navigate_to "$base_url/nhl/"
 sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 tap 450 1550
-swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
-swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
-swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 
 navigate_to "$base_url/binghuiliu/"
@@ -37,6 +34,7 @@ swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 
 navigate_to "$base_url/warriors/"
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 tap 450 1550
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
@@ -45,6 +43,7 @@ swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 
 navigate_to "$base_url/nba/"
+sleep $BASE_SUBPAGE_NAVIGATION_WAIT_TIME
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
 tap 450 1550
 swipe $swipe_params && sleep $BASE_SWIPE_WAIT_TIME
